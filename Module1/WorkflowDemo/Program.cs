902f226af9e790870c8184bfa34543e7e3191a4b
Zachery Alexander <zach8528alex@gmail.com>
2025-09-24 19:02:37 -0700

    Assignment 1 Work
    Includes all work done for assignment 1

---
+++ Module1/WorkflowDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Author: Zachery Alexander
+Date: 09/24/2025
+Assignment: Programming Assignment 1 (PA1)
+*/
+
+namespace WorkflowDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output introductory message
+            Console.WriteLine("Hello! I am now going to bark at you: ");
+
+            // Output barking sound using escape characters
+            Console.WriteLine("\"woof woof! arf ruff\"");
+
+            // Output closing message
+            Console.WriteLine("I am now done barking at you uwu");
+        }
+    }
+}
